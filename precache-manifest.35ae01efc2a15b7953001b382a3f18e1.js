bbeda9223537e4b8432b9e34a067109144ad7798
Vova <vovakoshelvgks@gmail.com>
2019-01-23 19:27:20 +0300

    Updates

---
+++ precache-manifest.35ae01efc2a15b7953001b382a3f18e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myreact/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f726a0ea515ef25c0f39",
+    "url": "/myreact/static/js/runtime~main.f726a0ea.js"
+  },
+  {
+    "revision": "c1a329c7fb034fd1ee13",
+    "url": "/myreact/static/js/main.c1a329c7.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/myreact/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "c1a329c7fb034fd1ee13",
+    "url": "/myreact/static/css/main.20ad6320.chunk.css"
+  },
+  {
+    "revision": "2bac6e656513254e8701733d7cc15b5a",
+    "url": "/myreact/index.html"
+  }
+];
